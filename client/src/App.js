7e86886caaaa9712a2c94184ea7694fa4d612eda
jialin wu <jialin.wu.02@gmail.com>
2019-08-20 19:29:47 -0700

    pie chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import magnifiertool from './img/magnifier-tool.png'
+import magnifiertool from './img/magnifier-tool.png';
+
+import BarChart from './components/BarChart';
 
 import './App.css';
 
 class App extends Component {
   state = {
+    searchFinish: false,
     address: '',
     location: '',
     weatherData: ''
@@ -24,25 +27,28 @@ class App extends Component {
     const body = await response.json();
     console.log(body)
     this.setState({ 
+      searchFinish: true,
       weatherData: body.forecast,
       location: body.location
     });
     console.log(this.state);
   };
   
-render() {
-    return (
-      <div className="main-content">
-        <Header title={"Weather"} />
-        <form className="search-form" onSubmit={this.handleSubmit}>
-            <input value={this.state.address} onChange={e => this.setState({ address: e.target.value })} className="search-input" type="text" />
-            <input img className="search-button" onClick={this.handleSubmit} src={magnifiertool} type="image" alt="submit" 
-            style={{width: "36px"}}/>
-        </form>
-        <Footer />
-      </div>
-    );
+  render() {
+      return (
+        <div className="main-content">
+          <Header title={this.state.location ? this.state.location : "Weather"} />
+          {!this.state.searchFinish ?           
+          <form className="search-form" onSubmit={this.handleSubmit}>
+              <input value={this.state.address} onChange={e => this.setState({ address: e.target.value })} className="search-input" type="text" />
+              <input img className="search-button" onClick={this.handleSubmit} src={magnifiertool} type="image" alt="submit" 
+              style={{width: "36px"}}/>
+          </form> : 
+          <BarChart /> }
+          <Footer />
+        </div>
+      );
+    }
   }
-}
 
 export default App;
