067ad303bc58f2e94595e5ce9fc4f31611a308fa
jialin wu <jialin.wu.02@gmail.com>
2019-08-21 16:38:05 -0700

    finish pie chart 2

--- client/src/components/PieChart.js
+++ client/src/components/PieChart.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import * as d3 from "d3";
 
-// import './PieChart.css';
-
 class PieChart extends Component {
 
 
   componentDidMount = () => {
-    var width = 1000
-    let height = 450
-    let margin = 10
-
-    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-    var radius = Math.min(width, height) / 2 - margin
+    var width = 500
+    let height = 600
 
     // append the svg object to the div called 'my_dataviz'
     var svg = d3.select("#PieChart")
@@ -20,31 +14,75 @@ class PieChart extends Component {
         .attr("width", width)
         .attr("height", height)
     .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        .attr("transform", "translate(" + width / 2+ "," + height / 2 + ")");
+
+    var svg1 = d3.select("#PieChart")
+    .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+    .append("g")
+        .attr("transform", "translate(" + width / 2+ "," + height / 2 + ")");
     
     // Create dummy data
-    var data = {a: 1, b: 1, c:1, d:1, e:1, f:1, g:1, h:1, i:1, j:1, k:1, l:1, m:1, n:1, o:1, p:1, q:1, r:1, s:1, t:1, u:1, v:1, w:1, x:1}
+    var data = {a: 1, b: 1, c:1, d:1, e:1, f:1, g:1, h:1, i:1, j:1, k:1, l: 1}
 
-    // set the color scale
-    var color = d3.scaleOrdinal()
-    .domain(data)
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+    // calculate current hour:
+    var date = new Date(this.props.hourly[0].time * 1000);
+    var hour = date.getHours();
 
     // calculate the average of the data:
     var sum = 0;
+    var sum1 = 0;
+    var sum2 = 0;
     for (let j = 0; j < 24; j++) {
       sum += this.props.hourly[j].temperature;
+      j < 12 ? sum1 += this.props.hourly[j].temperature : sum2 += this.props.hourly[j].temperature;
     }
+    var average1 = sum1 / 12;
+    var average2 = sum2 / 12;
     var average = sum / 24;
 
-    var dataHeightArray = []
-    for (let i = 0; i < 24; i++) {
-       dataHeightArray[i] = (this.props.hourly[i].temperature - average) * 8 + 190;
+    var dataHeightArray1 = []
+    var dataHeightArray2 = []
+
+    var weatherColorMAP = {
+      "clear-day": "#f5d442",
+      "clear-night": "#f5d442",
+      "cloudy": "#87fae3",
+      "partly-cloudy-day": "#87fae3",
+      "partly-cloudy-night": "#87fae3",
+      "rain": "#93cafa"
+    }
+    var weatherColor1 = [];
+    var weatherColor2 = [];
+    // adding height and color together
+    for (let i = 0; i < 12; i++) {
+      dataHeightArray1[(i + hour) % 12] = (this.props.hourly[i].temperature - average) * 8 + 190;
+      weatherColor1[(i + hour) % 12] = weatherColorMAP[(this.props.hourly[i].icon)]
+    }
+    for (let i = 12; i < 24; i++) {
+      dataHeightArray2[(i + hour) % 12] = (this.props.hourly[i].temperature - average) * 8 + 190;
+      weatherColor2[(i + hour) % 12] = weatherColorMAP[(this.props.hourly[i].icon)]
     }
 
-    var dataHeight = d3.scaleOrdinal()
+    var dataHeight1 = d3.scaleOrdinal()
     .domain(data)
-    .range(dataHeightArray)
+    .range(dataHeightArray1)
+  
+    var dataHeight2 = d3.scaleOrdinal()
+    .domain(data)
+    .range(dataHeightArray2)
+
+    // set the color scale
+    var color1 = d3.scaleOrdinal()
+    .domain(data)
+    .range(weatherColor1)
+
+    // set the color scale
+    var color2 = d3.scaleOrdinal()
+    .domain(data)
+    .range(weatherColor2)
+
 
     // Compute the position of each group on the pie:
     var pie = d3.pie()
@@ -59,9 +97,23 @@ class PieChart extends Component {
     .append('path')
     .attr('d', d3.arc()
         .innerRadius(130)
-        .outerRadius(function(d){ return (dataHeight(d.data.key))})
+        .outerRadius(function(d){ return (dataHeight1(d.data.key))})
     )
-    .attr('fill', function(d){ return(color(d.data.key)) })
+    .attr('fill', function(d){ return(color1(d.data.key)) })
+    .attr("stroke", "black")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.7)
+
+    svg1
+    .selectAll('whatever')
+    .data(data_ready)
+    .enter()
+    .append('path')
+    .attr('d', d3.arc()
+        .innerRadius(130)
+        .outerRadius(function(d){ return (dataHeight2(d.data.key))})
+    )
+    .attr('fill', function(d){ return(color2(d.data.key)) })
     .attr("stroke", "black")
     .style("stroke-width", "2px")
     .style("opacity", 0.7)
--- forecast.js
+++ forecast.js
@@ -1,6 +1,7 @@
 const request = require('request');
 
 const forecast = (longitude, latitude, callback) => {
+    // console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
     const url = "https://api.darksky.net/forecast/34301e9f6dfa336acf5d9ddb6b802548/" + latitude + "," + longitude  + "?units=si";
     request({ url, json: true}, (error, { body }) => {
         if (error) {
